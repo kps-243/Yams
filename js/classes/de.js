98b8b8fa71b665df0a45bd87500ba26880bf79fb
Rayan69100 <yayan1@live.fr>
2024-07-02 12:32:31 +0200

    maj du de

--- js/classes/de.js
+++ js/classes/de.js
@@ -1,14 +1,26 @@
 class Dice {
   valeur = null;
-  //ajouter un attribut valeur
   throw() {
     const result = Math.floor(Math.random() * 6) + 1;
     this.valeur = result;
     return this.valeur;
   }
   updateDiceImage(result) {
+    // Sélectionner toutes les balises img qui sont enfants d'un élément avec la classe "dices"
+    var images = document.querySelectorAll(".dices img");
+    console.log(images);
     var diceImage = document.getElementById("diceImage");
     diceImage.src = `../image/dice${result}.png`;
   }
 }
+
+document.getElementById("rollButton").addEventListener("click", throwDice);
+
+function throwDice() {
+  var dice = new Dice();
+  var resulatDice = dice.throw();
+  console.log(resulatDice);
+  var changeimage = dice.updateDiceImage(resulatDice);
+}
+
 export default Dice;
