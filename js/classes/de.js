f0c21824672c8958ab2dced191cb0534bfa4dcf3
Rayan69100 <yayan1@live.fr>
2024-07-02 15:11:06 +0200

    ajout de plusieurs dice

--- js/classes/de.js
+++ js/classes/de.js
@@ -1,32 +1,54 @@
 class Dice {
-  valeur = null;
+  constructor() {
+    this.valeur = this.throw;
+    this.imageElement = document.createElement("img");
+    this.imageElement.id = "diceImage";
+    this.imageElement.src = "../image/dice1.png";
+    this.imageElement.alt = "Dice";
+    this.imageElement.width = 300;
+    this.imageElement.height = 200;
+
+    document.querySelector(".dices").appendChild(this.imageElement);
+  }
+
   throw() {
     const result = Math.floor(Math.random() * 6) + 1;
-    this.valeur = result;
-    return this.valeur;
+    return result;
   }
-
-  updateDiceImage(result) {
-    // Sélectionner toutes les balises img qui sont enfants d'un élément avec la classe "dices"
-    var diceImageAll = document.querySelectorAll(".dices img");
-
-    //var diceImage = document.getElementById("diceImage");
-
-    diceImageAll.forEach((diceImage, index) => {
-      diceImage.src = `../image/dice${result}.png`;
-    });
-
-    //diceImage.src = `../image/dice${result}.png`;
+  updateDiceImage(value) {
+    this.imageElement.src = `../image/dice${value}.png`;
   }
+  // updateDiceImage(result) {
+  //   // Sélectionner toutes les balises img qui sont enfants d'un élément avec la classe "dices"
+  //   var diceImageAll = document.querySelectorAll(".dices img");
+
+  //   //var diceImage = document.getElementById("diceImage");
+
+  //   diceImageAll.forEach((diceImage, index) => {
+  //     diceImage.src = `../image/dice${result}.png`;
+  //   });
+
+  //   //diceImage.src = `../image/dice${result}.png`;
+  // }
 }
 
-document.getElementById("rollButton").addEventListener("click", throwDice);
+var dice = new Dice();
+var dice2 = new Dice();
+//document.getElementById("rollButton").addEventListener("click");
 
-function throwDice() {
-  var dice = new Dice();
-  var resulatDice = dice.throw();
-  console.log(resulatDice);
-  var changeimage = dice.updateDiceImage(resulatDice);
-}
+document.getElementById("rollButton").addEventListener("click", () => {
+  const value = dice.throw();
+  const value2 = dice2.throw();
+  dice.updateDiceImage(value);
+  dice2.updateDiceImage(value2);
+  console.log(value);
+});
+
+//function throwDice() {
+
+//var resulatDice = dice.throw();
+//console.log(resulatDice);
+// var changeimage = dice.updateDiceImage(resulatDice);
+//}
 
 export default Dice;
